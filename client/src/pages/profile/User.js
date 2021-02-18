dc3aa05b0750ddcd37d9ddd24adf2f43251799c8
Taylor Collins <taylorcc33@gmail.com>
2021-02-17 21:54:36 -0700

    removed excess code

--- client/src/pages/profile/User.js
+++ client/src/pages/profile/User.js
@@ -3,7 +3,7 @@ import { useState, useEffect, useContext } from "react";
 import { Button, Icon } from "semantic-ui-react";
 import "./style.css";
 import { AuthContext } from "../../providers/AuthProvider";
-import { Link, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 import UserProjects from "./UserProjects";
 import Requests from "./Requests";
 import Axios from "axios";
